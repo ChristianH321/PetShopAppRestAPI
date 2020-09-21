88868945e11a27b15307337592be7141d3d25d5e
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:06:30 +0200

    Create CustomerController.cs

---
+++ PetShopAppWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetShop.Core.ApplicationService;
+using PetShop.Core.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PetShopAppWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+    
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public List<Customer> Get()
+        {
+            return _customerService.GetCustomer();
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "None";
+            //return _customerService.FindCustomerById(id);
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
