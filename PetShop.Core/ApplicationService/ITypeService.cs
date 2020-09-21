c704c14c1a2154c1c42ddd01e3db14d51178c95c
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:03:00 +0200

    Create ITypeService.cs

---
+++ PetShop.Core/ApplicationService/ITypeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using PetShop.Core.Entities;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.ApplicationService
+{
+    public interface ITypeService
+    {
+
+        Entities.Type CreateType(Entities.Type ty);
+        Entities.Type DeleteType(int id);
+        List<Entities.Type> GetType();
+        void InitDatabaseDataType();
+        List<Entities.Type> getAllByType(string chosenType);
+        List<Entities.Type> GetTypes();
+        Entities.Type NewType(string typetype);
+        Entities.Type UpdateType(Entities.Type typeUpdate);
+
+
+
+
+    }
+}
