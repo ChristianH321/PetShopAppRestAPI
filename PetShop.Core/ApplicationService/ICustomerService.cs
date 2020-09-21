6250de208ec5f84c4ca5624d46bb97c2dcb87fe9
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:02:47 +0200

    Create ICustomerService.cs

---
+++ PetShop.Core/ApplicationService/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using PetShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.ApplicationService
+{
+    public interface ICustomerService
+    {
+        Customer CreateCustomer(Customer cu);
+        Customer DeleteCustomer(int id);
+        List<Customer> GetCustomer();
+        void InitDatabaseDataCustomer();
+        Customer NewCustomer(string firstName, string lastName, DateTime birthDateOfCustomer, string adress);
+        Customer UpdateCustomer(Customer customerUpdate);
+
+    }
+}
