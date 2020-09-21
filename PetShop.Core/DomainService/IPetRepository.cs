d1acf456bc531e3105da25a0b14efca3b0929f1b
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:03:55 +0200

    Create IPetRepository.cs

---
+++ PetShop.Core/DomainService/IPetRepository.cs
@@ -0,0 +1,17 @@
+﻿using PetShop.Core.ApplicationService;
+using PetShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.DomainService
+{
+    public interface IPetRepository
+    {
+        IEnumerable<Pet> ReadPets();
+        Pet Create(Pet pe);
+        Pet FindPetbyId(int id);
+        void IntialiseData();
+        Pet Delete(int id);
+    }
+}
