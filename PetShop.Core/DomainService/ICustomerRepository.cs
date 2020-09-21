56da1792bd4ef039257dd5333ec70ebf312cc747
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:03:30 +0200

    Create ICustomerRepository.cs

---
+++ PetShop.Core/DomainService/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using PetShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.DomainService
+{
+    public interface ICustomerRepository
+    {
+        Customer Create(Customer cu);
+        Customer Delete(int id);
+        IEnumerable<Customer> ReadCustomers();
+        void IntialiseData();
+        Customer FindCustomerbyId(int id);
+    }
+}
