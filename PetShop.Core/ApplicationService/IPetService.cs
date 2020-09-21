58c12ab4db14bb02e7e406a9cfa801b8802b2a40
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:02:53 +0200

    Create IPetService.cs

---
+++ PetShop.Core/ApplicationService/IPetService.cs
@@ -0,0 +1,21 @@
+﻿using PetShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.ApplicationService
+{
+    public interface IPetService
+    {
+        Pet CreatePet(Pet pe);
+        Pet DeletePet(int id);
+        Pet FindPetById(int id);
+        List<Pet> getASC();
+        List<Pet> getCheapest();
+        List<Pet> getDESC();
+        List<Pet> GetPets();
+        void InitDatabaseData();
+        Pet NewPet(string name, string previousOwner, double price, DateTime soldDate, DateTime birthDate, string color);
+        Pet UpdatePet(Pet petUpdate);
+    }
+}
