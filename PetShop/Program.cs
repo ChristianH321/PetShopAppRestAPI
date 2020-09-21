5c2b6c8afb360d76ab537cd111fdc6adc0f14ad9
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:06:06 +0200

    Create Program.cs

---
+++ PetShop/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetShop.Core.ApplicationService;
+using PetShop.Core.DomainService;
+using PetShop.Infrastructure.Data.Repository;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace PetShop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<IPetRepository, PetRepository>();
+            serviceCollection.AddScoped<IPetService, PetService>();
+            serviceCollection.AddScoped<IPrinter, Printer>();
+            serviceCollection.AddScoped<ICustomerRepository, CustomerRepository>();
+            serviceCollection.AddScoped<ICustomerService, CustomerService>();
+            serviceCollection.AddScoped<ITypeRepository, TypeRepository>();
+            serviceCollection.AddScoped<ITypeService, TypeService>();
+
+            // then build provider
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var printer = serviceProvider.GetRequiredService<IPrinter>();
+            printer.StartUI();
+
+
+        }
+    }
+}
