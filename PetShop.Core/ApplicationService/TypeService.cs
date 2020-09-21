67b9477db37262741c50412e13b6566cad0a488a
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:03:17 +0200

    Create TypeService.cs

---
+++ PetShop.Core/ApplicationService/TypeService.cs
@@ -0,0 +1,84 @@
+﻿using PetShop.Core.DomainService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetShop.Core.ApplicationService
+{
+    public class TypeService: ITypeService
+    {
+
+        private ITypeRepository _typeRepo;
+
+        public TypeService(ITypeRepository perReposit)
+        {
+            _typeRepo = perReposit;
+        }
+
+
+        public void InitDatabaseDataType()
+        {
+            _typeRepo.IntialiseData();
+        }
+
+        public Entities.Type FindTypeById(int id)
+        {
+            return _typeRepo.FindTypebyId(id);
+        }
+
+
+        public Entities.Type CreateType(Entities.Type ty)
+        {
+            return _typeRepo.Create(ty);
+        }
+
+        public Entities.Type DeleteType(int id)
+        {
+            return _typeRepo.Delete(id);
+        }
+
+        public List<Entities.Type> GetType()
+        {
+            return _typeRepo.ReadTypes().ToList();
+        }
+
+        public List<Entities.Type> getAllByType(string chosenType)
+        {
+            var allTypes = GetTypes();
+            List<Entities.Type> searchedListOfTypes = new List<Entities.Type>();
+            foreach (var type in allTypes)
+            {
+                if (type.TypeType.Contains(chosenType))
+                {
+                    searchedListOfTypes.Add(type);
+                }
+            }
+
+            return searchedListOfTypes;
+        }
+
+        public List<Entities.Type> GetTypes()
+        {
+            return _typeRepo.ReadTypes().ToList();
+        }
+
+        public Entities.Type NewType(string typetype)
+        {
+            var ty = new Entities.Type()
+            {
+                TypeType = typetype,
+            };
+
+            return ty;
+        }
+
+        public Entities.Type UpdateType(Entities.Type typeUpdate)
+        {
+            var type = FindTypeById(typeUpdate.ID);
+            type.TypeType = typeUpdate.TypeType;
+
+            return type;
+        }
+    }
+}
