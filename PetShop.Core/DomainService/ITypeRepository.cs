f7e3eddcccda51d795dc8e4b92163a015c2cc4e6
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:04:15 +0200

    Create ITypeRepository.cs

---
+++ PetShop.Core/DomainService/ITypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PetShop.Core.Entities;
+
+namespace PetShop.Core.DomainService
+{
+    public interface ITypeRepository
+    {
+        Entities.Type Create(Entities.Type ty);
+        Entities.Type Delete(int id);
+        void IntialiseData();
+        IEnumerable<Entities.Type> ReadTypes();
+        Entities.Type FindTypebyId(int id);
+        IEnumerable<Entities.Type> ReadType();
+    }
+}
