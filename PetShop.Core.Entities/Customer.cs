3562b4545859840e69329c2498a2038650ae1da3
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:02:15 +0200

    Create Customer.cs

---
+++ PetShop.Core.Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.Entities
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDateOfCustomer { get; set; }
+        public string Adress { get; set; }
+
+
+    }
+}
