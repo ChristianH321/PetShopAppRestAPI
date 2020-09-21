194f2503085efefde501b1b583de8816a360bbcf
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:05:27 +0200

    Create PetRepository.cs

---
+++ PetShop.Infrastructure.Data/Repository/PetRepository.cs
@@ -0,0 +1,61 @@
+﻿using PetShop.Core.DomainService;
+using PetShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Infrastructure.Data.Repository
+{
+    public class PetRepository: IPetRepository
+    {
+        static int id = 1;
+        private List<Pet> _pets = new List<Pet>();
+
+        public Pet Create(Pet pe)
+        {
+            pe.ID = id++;
+            _pets.Add(pe);
+            return pe;
+        }
+
+        public Pet Delete(int id)
+        {
+            var petFound = this.FindPetbyId(id);
+            if(petFound != null)
+            {
+                _pets.Remove(petFound);
+                return petFound;
+            }
+            return null;
+        }
+
+        public Pet FindPetbyId(int id)
+        {
+            foreach (var pet in _pets)
+            {
+                if(pet.ID == id)
+                {
+                    return pet;
+                }
+            }
+
+            return null;
+        }
+
+        public void IntialiseData()
+        {
+            _pets = FakeDB.InitDataPet();
+            IDAfterInit();
+        }
+
+        private void IDAfterInit()
+        {
+            id = FakeDB.getID();
+        }
+
+        public IEnumerable<Pet> ReadPets()
+        {
+            return _pets;
+        }
+    }
+}
