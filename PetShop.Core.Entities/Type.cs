dddd15c55ff74d0280b61d46f8939acb39ecdd63
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-09-21 17:02:34 +0200

    Create Type.cs

---
+++ PetShop.Core.Entities/Type.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.Entities
+{
+    public class Type
+    {
+        public int ID { get; set; }
+        public string TypeType { get; set; }
+
+    }
+}
