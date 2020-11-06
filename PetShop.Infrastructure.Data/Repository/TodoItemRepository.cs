7dfccb05f479b2098c6426a33eadc466ad5b458b
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2020-11-06 09:46:19 +0100

    Changes

--- PetShop.Infrastructure.Data/Repository/TodoItemRepository.cs
+++ PetShop.Infrastructure.Data/Repository/TodoItemRepository.cs
@@ -1,6 +1,8 @@
-﻿using PetShop.Core.DomainService;
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Core.DomainService;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using ToDoApiAuthentication;
 
